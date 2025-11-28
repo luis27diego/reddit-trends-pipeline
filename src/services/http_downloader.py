bbe412046dce08f2679aeb0e96033a1db8f2e3e8
luis27diego <yeye.rodriguez.27@gmail.com>
2025-11-28 14:52:46 -0300

    Refactor download_file function to remove CSV reading and return content from URL

--- src/services/http_downloader.py
+++ src/services/http_downloader.py
@@ -1,17 +1,20 @@
 import requests
-import pandas as pd
 
+def download_file(url: str) -> bytes:
+    response = requests.get(url, stream=True, timeout=60)
+    response.raise_for_status()
+    return response.content
 
-def download_file(url: str = None) -> bytes:
-    url = url
-    # Leer el archivo CSV
-    df = pd.read_csv(r"C:\Users\luisd\Downloads\archive\the-reddit-covid-dataset-posts.csv")
+# def download_file(url: str = None) -> bytes:
+#     url = url
+#     # Leer el archivo CSV
+#     df = pd.read_csv(r"C:\Users\luisd\Downloads\archive\the-reddit-covid-dataset-posts.csv")
     
-    # Extraer la columna 'selftext', asegurándonos de que no haya valores nulos
-    selftext_column = df['selftext'].dropna()
+#     # Extraer la columna 'selftext', asegurándonos de que no haya valores nulos
+#     selftext_column = df['selftext'].dropna()
 
-    # Unir todos los textos en un solo string
-    combined_text = "\n".join(selftext_column)
+#     # Unir todos los textos en un solo string
+#     combined_text = "\n".join(selftext_column)
 
-    # Convertir el texto combinado a bytes
-    return combined_text.encode('utf-8')
+#     # Convertir el texto combinado a bytes
+#     return combined_text.encode('utf-8')
