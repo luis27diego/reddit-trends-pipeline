99684a35550eb1c0150a0a1a8ed591560752c224
luis27diego <yeye.rodriguez.27@gmail.com>
2025-11-28 13:24:07 -0300

    Refactor Spark session creation for improved clarity and organization

--- src/services/spark_service.py
+++ src/services/spark_service.py
@@ -1,24 +1,37 @@
+# src/services/spark_service.py
 from pyspark.sql import SparkSession
 from src.config.spark_config import SPARK_MASTER_URL, APP_NAME
 
 def create_spark_session():
-    """
-    Crea una SparkSession conectada al cluster Docker Spark Standalone.
-    """
     spark = (
         SparkSession.builder
         .appName(APP_NAME)
         .master(SPARK_MASTER_URL)
-        .config("spark.driver.maxResultSize", "2g")    # importante para evitar OOM en collect()
-        # ---- Particiones agresivas (clave para baja memoria) ----
-        .config("spark.sql.shuffle.partitions", "400")     # muchas particiones = menos memoria por tarea
+        .config("spark.driver.maxResultSize", "2g")
+        .config("spark.sql.shuffle.partitions", "400")
         .config("spark.default.parallelism", "400")
-        .config("spark.sql.files.maxPartitionBytes", "64mb")   # particiones pequeñas
-        .config("spark.sql.adaptive.enabled", "true")         # Spark 3+ optimiza solo
+        .config("spark.sql.files.maxPartitionBytes", "64mb")
+        .config("spark.sql.adaptive.enabled", "true")
         .config("spark.sql.adaptive.coalescePartitions.enabled", "true")
-        # ---- Optimizaciones para texto grande ----
         .config("spark.rdd.compress", "true")
         .config("spark.speculation", "false")
+        
+        #Configuración MinIO
+        .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000")
+        .config("spark.hadoop.fs.s3a.access.key", "minioadmin")
+        .config("spark.hadoop.fs.s3a.secret.key", "minioadmin")
+        
+        # --- SOLUCIÓN DEL ERROR 403 ---
+        # 1. Fuerza el uso de las claves definidas arriba, ignora variables de entorno
+        .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
+        
+        # 2. Necesario para MinIO (evita errores de DNS)
+        .config("spark.hadoop.fs.s3a.path.style.access", "true")
+        
+        # 3. Definición de clase (por seguridad)
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+        .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
+        
         .getOrCreate()
     )
     return spark
