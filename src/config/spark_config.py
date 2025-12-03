5e3ee8dfc09d9d9428a77df2d78e444cf30b146c
luis27diego <yeye.rodriguez.27@gmail.com>
2025-12-03 13:41:22 -0300

    Refactor configuration to use environment variables for MinIO, Spark, and PostgreSQL; remove unused config files.

--- src/config/spark_config.py
+++
@@ -1,6 +0,0 @@
-SPARK_MASTER_URL = "spark://spark-master:7077"
-
-APP_NAME = "reddit-trends-processing"
-
-# Carpeta interna en MinIO donde se guardan los resultados procesados
-PROCESSED_FOLDER = "processed"
