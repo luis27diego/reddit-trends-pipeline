e4d2e493afd04f8562cf0b7518ccb03466dd5984
luis27diego <yeye.rodriguez.27@gmail.com>
2025-12-04 11:06:59 -0300

    Refactor flujo_ingesta and tasks_ingesta: enable crear_directorio and update descargar_reddit_dump to accept URL parameter

--- flows/ingesta/tasks_ingesta.py
+++ flows/ingesta/tasks_ingesta.py
@@ -8,20 +8,21 @@ def crear_directorio():
     pass  # si luego lo necesitas
 
 @task(retries=1, retry_delay_seconds=10)
-async def descargar_reddit_dump(dataset_slug: str = "pavellexyr/the-reddit-climate-change-dataset",target_file: str = "the-reddit-climate-change-dataset-comments.csv"):
+async def descargar_reddit_dump(url: str):
+    file_name = url.split("/")[-1]
+    key = f"{settings.RAW_FOLDER}/{file_name}"
+
     bucket = await get_minio_bucket()
-    minio_key = f"{settings.RAW_FOLDER}/{target_file}"
-    print(f"Verificando si el archivo ya existe en MinIO: {minio_key}")
-    # Check if already exists in MinIO
-    if await file_exists(bucket, minio_key):
-        print(f"Archivo ya existe en MinIO: {minio_key}")
-        return minio_key
-    # url = "vv"
-    # print(f"Descargando dataset desde {url}")
-    # content = download_file(url)
 
-    # print("Subiendo archivo a MinIO...")
-    # await upload_file(bucket, key, content)
+    if await file_exists(bucket, key):
+        print(f"Archivo ya existe: {key}")
+        return key
 
-    # print(f"Archivo subido: {key}")
-    return None
+    print(f"Descargando dataset desde {url}")
+    content = download_file(url)
+
+    print("Subiendo archivo a MinIO...")
+    await upload_file(bucket, key, content)
+
+    print(f"Archivo subido: {key}")
+    return key
