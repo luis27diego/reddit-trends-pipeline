262b45bd7af5d41201f2cdbbc658a6943193f275
luis27diego <yeye.rodriguez.27@gmail.com>
2025-12-04 10:39:05 -0300

    pasado_Test

--- flows/ingesta/tasks_ingesta.py
+++ flows/ingesta/tasks_ingesta.py
@@ -2,23 +2,27 @@ from prefect import task
 from src.infrastructure.storage.minio_client import get_minio_bucket, file_exists, upload_file
 from src.infrastructure.http.client import download_file
 from src.config.settings import settings
-from src.infrastructure.kaggle.downloader import download_dataset, get_csv_files    
 
 @task
 def crear_directorio():
     pass  # si luego lo necesitas
 
 @task(retries=1, retry_delay_seconds=10)
-async def descargar_reddit_dump(dataset_slug: str = "pavellexyr/the-reddit-climate-change-dataset", target_file: str = "the-reddit-climate-change-dataset-comments.csv"):
+async def descargar_reddit_dump(url: str):
+    file_name = url.split("/")[-1]
+    key = f"{settings.RAW_FOLDER}/{file_name}"
 
-
-    minio_key = f"{settings.RAW_FOLDER}/{target_file}"
     bucket = await get_minio_bucket()
 
-    if await file_exists(bucket, minio_key):
-        print(f"Archivo ya existe: {minio_key}")
-        return minio_key
+    if await file_exists(bucket, key):
+        print(f"Archivo ya existe: {key}")
+        return key
 
-    print(f"Descargando dataset desde Kaggle: {dataset_slug}")
+    print(f"Descargando dataset desde {url}")
+    content = download_file(url)
 
-    return minio_key
+    print("Subiendo archivo a MinIO...")
+    await upload_file(bucket, key, content)
+
+    print(f"Archivo subido: {key}")
+    return key
